1f93422247ef7a2c77609702e1ba162dfcada3ab
nickTaMaire <nicolasblanc46@gmail.com>
2022-05-02 21:18:08 +0200

    change first view

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
-import 'package:coi_panel/Admin.dart';
+import 'package:coi_panel/AddSubs.dart';
+import 'package:coi_panel/Home.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -8,24 +9,15 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const AddSubView(),
+      home: const HomeView(),
     );
   }
 }
