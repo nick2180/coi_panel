42edb3b75493469126970d99b262b7ba7f44010d
nickTaMaire <nicolasblanc46@gmail.com>
2022-05-02 23:09:38 +0200

    start firebase with app

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,21 @@
-import 'package:coi_panel/AddSubs.dart';
 import 'package:coi_panel/Home.dart';
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
+import 'package:firebase_core/firebase_core.dart';
+
+Future main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+      options: const FirebaseOptions(
+          apiKey: "AIzaSyD_aVeZDwg6toU9K6DdO9dLLvMiDgUHbZw",
+          authDomain: "connecetmyhobbies.firebaseapp.com",
+          projectId: "connecetmyhobbies",
+          storageBucket: "connecetmyhobbies.appspot.com",
+          messagingSenderId: "359478760391",
+          appId: "1:359478760391:web:a6cd4be02a184d1d5bbfe2",
+          measurementId: "G-NTPW7JL70N"));
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
