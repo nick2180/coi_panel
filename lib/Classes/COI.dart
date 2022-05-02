1534a6da26cf2593e1c6a3284f4b636a193e2aea
nickTaMaire <nicolasblanc46@gmail.com>
2022-05-03 01:01:50 +0200

    remove useless field

--- lib/Classes/COI.dart
+++ lib/Classes/COI.dart
@@ -4,22 +4,26 @@ class COI {
   bool isReviewed;
 
   int followers;
+  int subs;
 
   COI({
     required this.coi,
     required this.isReviewed,
     required this.followers,
+    required this.subs,
   });
 
   Map<String, dynamic> toJson() => {
         'coi': coi,
         'isReviewed': isReviewed,
         'followers': followers,
+        'subs': subs,
       };
 
   static COI fromJson(Map<String, dynamic> json) => COI(
         coi: json['coi'],
         isReviewed: json['isReviewed'],
         followers: json['followers'],
+        subs: json['subs'],
       );
 }
