0561b5edd01498cfb0211eab838fc79309c19ffb
nickTaMaire <nicolasblanc46@gmail.com>
2022-05-03 11:00:55 +0200

    select app or web

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,28 @@
+import 'dart:io';
+
 import 'package:coi_panel/Home.dart';
 import 'package:flutter/material.dart';
 
 import 'package:firebase_core/firebase_core.dart';
 
 Future main() async {
+  bool web = true;
   WidgetsFlutterBinding.ensureInitialized();
 
-  await Firebase.initializeApp(
-      options: const FirebaseOptions(
-          apiKey: "AIzaSyD_aVeZDwg6toU9K6DdO9dLLvMiDgUHbZw",
-          authDomain: "connecetmyhobbies.firebaseapp.com",
-          projectId: "connecetmyhobbies",
-          storageBucket: "connecetmyhobbies.appspot.com",
-          messagingSenderId: "359478760391",
-          appId: "1:359478760391:web:a6cd4be02a184d1d5bbfe2",
-          measurementId: "G-NTPW7JL70N"));
+  if (web == false) {
+    await Firebase.initializeApp();
+  } else {
+    await Firebase.initializeApp(
+        options: const FirebaseOptions(
+            apiKey: "AIzaSyD_aVeZDwg6toU9K6DdO9dLLvMiDgUHbZw",
+            authDomain: "connecetmyhobbies.firebaseapp.com",
+            projectId: "connecetmyhobbies",
+            storageBucket: "connecetmyhobbies.appspot.com",
+            messagingSenderId: "359478760391",
+            appId: "1:359478760391:web:a6cd4be02a184d1d5bbfe2",
+            measurementId: "G-NTPW7JL70N"));
+  }
+
   runApp(MyApp());
 }
 
